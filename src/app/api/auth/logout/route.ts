db834484c51e6df99fa7682bc3f30786b3a23f43
Brayan Mesquita <brayanmesquita@gmail.com>
2025-10-25 15:33:35 -0400

    fix: remove sensitive data and update gitignore

--- src/app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { signOut } from "next-auth/react";
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  await signOut({ redirect: false });
-  return NextResponse.json({ message: "Logout realizado com sucesso" });
-}
